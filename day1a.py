fc7cef6305f7e18be55d63c3593a4c6d8d0e1a44
rzhlm <118811618+rzhlm@users.noreply.github.com>
2025-12-01 17:09:17 +0100

    day 1a: solved

--- day1a.py
+++ day1a.py
@@ -20,12 +20,14 @@ def rest_position(start: int = 50, instruction: str = "L0") -> int:
         num_clicks = int(instruction[1:])
         end = start - num_clicks
         if end < 0:
-            end += 100
+            while end < 0:
+                end += 100
     elif instruction[0] == "R":
         num_clicks = int(instruction[1:])
         end = start + num_clicks
         if end > 99:
-            end -= 100
+            while end > 99:
+                end -= 100
     else:
         raise Exception("rest_position: instruction: input not valid")
     print(f"{start=}; {num_clicks=}; {end=}")
@@ -35,6 +37,17 @@ def is_zero(position) -> int:
     return position == 0
 
 
+def give_zeroes(data: tuple[str]) -> int:
+    counter: int = 0
+    start: int = 50
+    for datum in data:
+        result = rest_position(start=start, instruction=datum)
+        if result == 0:
+            counter += 1
+        start = result
+    return counter
+
+
 def test():
     test_data = (
         "L68", "L30", "R48","L5","R60","L55","L1","L99","R14","L82"
@@ -42,12 +55,17 @@ def test():
     assert(rest_position(50, test_data[0]) == 82)
     assert(rest_position(82, test_data[1]) == 52)
     assert(rest_position(52, test_data[2]) == 0)
-    
+
+    assert(give_zeroes(test_data[:3]) == 1)
 
 
 def main():
     test()
-    pass
+    with open("./day1a-data.txt","r") as f:
+        lines = [line.strip() for line in f]
+    password = give_zeroes(lines)
+    print(f"{password=}")
+    
 
 if __name__ == "__main__":
     main()
