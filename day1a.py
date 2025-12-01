b91b1bbc1da87dfd7a690da63922afbb976b0492
rzhlm <118811618+rzhlm@users.noreply.github.com>
2025-12-01 16:52:35 +0100

    basic instructions are working

--- day1a.py
+++ day1a.py
@@ -10,8 +10,26 @@
 # number of times the dial has pointed at 0, after each rotation
 # 
 
-def rest_position(instruction: str) -> int:
-    pass
+def rest_position(start: int = 50, instruction: str = "L0") -> int:
+    """
+       input: start position (def: 50) & line instruction (def: do nothing)
+       output: resting position 
+    """
+    print(f"rest_pos() called with: {start=}; {instruction=}")
+    if instruction[0] == "L":
+        num_clicks = int(instruction[1:])
+        end = start - num_clicks
+        if end < 0:
+            end += 100
+    elif instruction[0] == "R":
+        num_clicks = int(instruction[1:])
+        end = start + num_clicks
+        if end > 99:
+            end -= 100
+    else:
+        raise Exception("rest_position: instruction: input not valid")
+    print(f"{start=}; {num_clicks=}; {end=}")
+    return end
 
 def is_zero(position) -> int:
     return position == 0
@@ -21,11 +39,15 @@ def test():
     test_data = (
         "L68", "L30", "R48","L5","R60","L55","L1","L99","R14","L82"
     )
-    assert()
+    assert(rest_position(50, test_data[0]) == 82)
+    assert(rest_position(82, test_data[1]) == 52)
+    assert(rest_position(52, test_data[2]) == 0)
+    
 
 
 def main():
     test()
+    pass
 
 if __name__ == "__main__":
     main()
